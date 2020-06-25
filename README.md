2b8cd3ad2724aecc7161107ffc285ad4981ce118
Zhang1933 <193319076@qq.com>
2020-06-25 22:25:43 +0800

    增加了使用说明 对于每个爬到的数据中间去掉了换行符

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 &ensp;&ensp;通过分析数据接口爬取豆瓣中评分9分以上的美剧的名字，评分，详情链接。并保存在文件'douban_drama.csv'中.✔
+
+# 爬取
+进入有pipfile的那个文件的目录依次输入：
+```batch
+pipenv shella
+scrapy crawl drama
+```
+就可以在当前目录下看到的数据爬取保存在了'douban_drama.csv'文件下了。
--- douban/douban/pipelines.py
+++ douban/douban/pipelines.py
@@ -10,13 +10,14 @@ class DoubanPipeline(object):
     # print("#"*50)
     def __init__(self):
         # print("#"*50)
-        f=open('douban_drama.csv','w',encoding='utf-8')
+        f=open('douban_drama.csv','w',newline='',encoding='utf-8')
         fieldnames=[
-            'title','rate','url'
+            'title','url','rate'
         ]
         self.writer = csv.DictWriter(f, fieldnames=fieldnames)
         self.writer.writeheader()
-    
+        
     def process_item(self, item, spider):
+        
         self.writer.writerow(item)
         return item
